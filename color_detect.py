87391e6cca023a17dc5d8d64d9afd0631aa50a1f
k2room <kimyirum@gm.gist.ac.kr>
2023-12-05 11:22:24 +0100

    test 2

--- color_detect.py
+++ color_detect.py
@@ -34,33 +34,47 @@ def relative_distance(dist, pos, FOV, stream_size):
 
     return [dist, real_pos]
 
-def send_msg(dist, pos, start):
-    state = 'x'
-    threshold = 35
+def send_msg(dist, pos, start, last_turn):
+    threshold = 60
     # turn left until detect object
     if start and dist == -777 and pos == -777:
-        state = 't'
+        state = 'k'
+    # turn left untsil detect object
+    elif dist == -777 and pos == -777 and (last_turn=='k' or last_turn=='l'):
+        state = 'e'
+    elif dist == -777 and pos == -777 and (last_turn=='e' or last_turn=='r'):
+        state = 'k'
     # straight
-    elif dist >= 20 and abs(pos) <= threshold:
+    elif dist >= 150 and abs(pos) <= 120:
         state = 'g'
+    # straight
+    elif dist < 150 and dist >= 20 and abs(pos) <= 60:
+        state = 'g'
+
     # turn left
-    elif pos < -threshold:
-        state = 't'
+    elif dist >= 150 and pos < -120:
+        state = 'l'
     # turn right
-    elif pos > threshold:
-        state = 't'
+    elif dist >= 150 and pos > 120:
+        state = 'r'
+    # turn left
+    elif dist < 150 and pos < -60:
+        state = 'l'
+    # turn right
+    elif dist < 150 and pos > 60:
+        state = 'r'
     # stop
     elif dist < 20:
         state = 'x'
-
+    
     return state
 
-def stream(ser):
+def stream(ser, use_window):
 
     FPS = 30
     stream_size = [640, 480]
     FOV = [87, 58]
-    msg_period = 1
+    msg_period = 5
 
     # Configure depth and color streams
     pipeline = rs.pipeline()
@@ -94,9 +108,9 @@ def stream(ser):
     pipeline.start(config)
 
     try:
-        msg_period_temp = msg_period
         start = True
         msg_seq = []
+        last_turn = ''
         while True:
             # Wait for a coherent pair of frames: depth and color
             frames = pipeline.wait_for_frames()
@@ -115,7 +129,7 @@ def stream(ser):
 
             # Red color mask
             lower_red = np.array([45, 20, 105]) # 빨간색 범위의 하한
-            upper_red = np.array([60, 35, 255]) # 빨간색 범위의 상한
+            upper_red = np.array([70, 35, 255]) # 빨간색 범위의 상한
             red_mask = cv2.inRange(color_image, lower_red, upper_red)
 
             # 모폴로지 연산으로 노이즈 제거
@@ -146,16 +160,22 @@ def stream(ser):
                 # print("Average Depth of Red Area: {:.2f}mm".format(average_depth), end='\r')
                 # print("Relative Coordinates(cm): {:.2f} (distance), {:.2f} (position)".format(coord[0]/10, coord[1]/10), end='\r')
                 # print("Relative Coordinates(cm): {:.2f} (distance), {:.2f} (position)".format(coord[0]/10, coord[1]/10))
-                message = send_msg(coord[0]/10, coord[1]/10, start)
+                message = send_msg(coord[0]/10, coord[1]/10, start, last_turn)
+                # if (message == 'k') or (message == 'e') or (message == 'l') or (message == 'r'):
+                if coord[1]/10 > 0:
+                    last_turn = 'k'
+                else:
+                    last_turn = 'e'
                 start = False
             else:
                 # print("Average Depth of Red Area: No red area detected.", end='\r')
-                print("Average Depth of Red Area: No red area detected.")
-                message = send_msg(-777, -777, start)
+                print("Average Depth of Red Area: No red area detected. last_turn:", last_turn)
+                message = send_msg(-777, -777, start, last_turn)
             msg_seq.append(message)
 
-            if len(msg_seq) == 5:
+            if len(msg_seq) == msg_period:
                 print("SEND MSG:", max(msg_seq).encode('utf-8'))
+
                 if ser !='':
                     ser.write(max(msg_seq).encode('utf-8'))
                 msg_seq = []
@@ -172,8 +192,9 @@ def stream(ser):
                 # images = np.vstack((np.hstack((color_image, depth_colormap)), np.hstack((red_image, red_depth))))
 
             # Show images
-            cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
-            cv2.imshow('RealSense', images)
+            if use_window:
+                cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
+                cv2.imshow('RealSense', images)
 
             if cv2.waitKey(1) & 0xFF == 27: # ESC 키
                 break
@@ -181,17 +202,19 @@ def stream(ser):
     finally:
         # Stop streaming
         pipeline.stop()
-        cv2.destroyWindow('RealSense')
+        if use_window:
+            cv2.destroyWindow('RealSense')
     
 
 if __name__=="__main__":
     use_serial = True
+    use_window = True
     if use_serial:
         ser = serial_connect()
         while ser == "no serial":
             ser = serial_connect()
             print("serial:", ser)
-        stream(ser)
+        stream(ser, use_window)
     else:
         ser = ''
-        stream(ser)
+        stream(ser, use_window)
